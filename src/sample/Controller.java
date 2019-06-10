88aec2bcefb77cf29d1d02302bbc721e333811f7
jonathanvo <jonatnv1@uci.edu>
2019-06-10 12:58:46 -0700

    Create GUI and controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,41 @@
 package sample;
 
-public class Controller {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    public ImageView imageView_1;
+    public ImageView imageView_2;
+    public ImageView imageView_3;
+    public ImageView imageView_4;
+    public ImageView imageView_5;
+    public ImageView imageView_6;
+    public ImageView imageView_7;
+    public ImageView imageView_8;
+    public ImageView imageView_9;
+    public ImageView imageView_10;
+    public ImageView imageView_11;
+    public ImageView imageView_12;
+    public Label moves_label;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+    }
+
+
+    public void imageView_clicked(MouseEvent mouseEvent) {
+
+    }
+
+    public void resetButton_pressed(ActionEvent actionEvent) {
+    }
 }
